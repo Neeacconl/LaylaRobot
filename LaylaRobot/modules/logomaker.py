c2c02e18434f001d6ef4ef17356c1b96b8216b6b
Conespoxs <87117498+Neeacconl@users.noreply.github.com>
2021-07-14 13:21:18 +0530

    Update logomaker.py

--- LaylaRobot/modules/logomaker.py
+++ LaylaRobot/modules/logomaker.py
@@ -100,11 +100,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByLayla.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaList Support @AwesomeSupport")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @katsurobot Support @KatsuSupport")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @AwesomeSupport, {e}')
+   await event.reply(f'Error Report @KatsuSupport, {e}')
 
 
 
@@ -140,7 +140,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByLyala.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @LaylaRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @katsurobot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
